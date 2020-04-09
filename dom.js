dd35b806a2c669a09e517cde79f205a2b2e9ea7f
mrnobodyanon <torikussadikk@gmail.com>
2020-04-09 12:14:24 +0600

    tor'

---
+++ dom.js
@@ -0,0 +1,55 @@
+
+//example of document show
+// console.dir(document);
+
+// show domain and url
+// console.dir(document.domain);
+// console.dir(document.URL);
+
+// show document title
+// console.dir(document.title);
+
+// to change title
+// document.title = 'torikus';
+
+// to view  DOCTYPE  
+// console.log(document.doctype);
+
+// to view head
+// console.log(document.head);
+
+// to view body
+// console.log(document.body);
+
+// to view all
+// console.log(document.all);
+
+// to access form
+// console.log(document.forms);
+
+// to access links
+// console.log(document.links);
+
+// to access images
+// console.log(document.images);
+
+// Get element by ID
+// console.log(document.getElementById('header-title'));
+
+// Get element by ID with variable
+// var headertitle = document.getElementById('header-title');
+// console.log(headertitle);
+
+// Get element by class name
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello 2';
+// items[1].getElementsByClassName.fontWeight='bold';
+// items[1].style.backgroundColor = 'yellow';
+
+
+
+
+
+
